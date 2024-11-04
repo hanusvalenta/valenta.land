6ee7317ffe7bb1c54548768544d986274789e96b
Hanuš Valenta <93769207+hanusvalenta@users.noreply.github.com>
2024-11-04 08:55:06 +0100

    fix?

--- Projects/Tools/Countdown/countdown.js
+++ Projects/Tools/Countdown/countdown.js
@@ -1,19 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var countDownDate = new Date("December 10, 2024 23:59:59").getTime();
+    const countDownDate = new Date("December 10, 2024 23:59:59").getTime();
 
-    var x = setInterval(function () {
-        var now = new Date().getTime();
+    const x = setInterval(function () {
+        const now = new Date().getTime();
+        const distance = countDownDate - now;
 
-        var distance = countDownDate - now;
+        if (distance >= 0) {
+            const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-        document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-
-        if (distance < 0) {
+            document.getElementById("countdown").innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+        } else {
             clearInterval(x);
             document.getElementById("countdown").innerHTML = "EXPIRED";
         }
@@ -22,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function () {
     fetch('DailyCopypastaStatus.txt')
         .then(response => response.text())
         .then(text => {
-            document.getElementById("countdown").innerHTML += "<br>" + text;
+            document.getElementById("countdown").innerHTML += `<br>${text}`;
         })
-        .catch(error => console.error('Error fetching copypasta.txt:', error));
+        .catch(error => console.error('Error fetching DailyCopypastaStatus.txt:', error));
 });
