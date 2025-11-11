627f28c8009c9d9dd2c2e94b01c73833798d3aa5
Hanuš Valenta <hanus.valenta@protonmail.com>
2025-11-11 11:55:43 +0100

    Update countdown date to December 11, 2025

--- Projects/Tools/Countdown/countdown.js
+++ Projects/Tools/Countdown/countdown.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () { 
-    const countDownDate = new Date("2025-05-25T00:00:00").getTime();
+    const countDownDate = new Date("2025-12-11T00:00:00").getTime();
     const countdownElement = document.getElementById("countdown");
     
     let rotationAngle = 0;
@@ -44,3 +44,4 @@ document.addEventListener('DOMContentLoaded', function () {
         })
         .catch(error => console.error('Error fetching DailyCopypastaStatus.txt:', error));
 });
+
